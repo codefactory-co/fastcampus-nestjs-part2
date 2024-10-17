37efc416419cad7f6039e2a6f5e516de18d18df1
Jiho Choi <withj1123@gmail.com>
2024-10-18 01:19:32 +0900

    프로젝트 구현하기 01 06 회원가입 구현하기

--- apps/user/src/user/user.service.ts
+++ apps/user/src/user/user.service.ts
@@ -1,8 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ){}
+
+  async create(createUserDto: CreateUserDto){
+    const {email, password} = createUserDto;
+
+    const user = await this.userRepository.findOne({
+      where:{
+        email,
+      }
+    });
+
+    if(user){
+      throw new BadRequestException('이미 가입한 이메일 입니다!')
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await this.userRepository.save({
+      ...createUserDto,
+      email,
+      password: hash,
+    });
+
+    return this.userRepository.findOne({
+      where:{
+        email,
+      }
+    });
   }
 }
