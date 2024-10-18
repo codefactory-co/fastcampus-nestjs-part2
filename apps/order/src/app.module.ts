104551f832d8dac821696e08b345a10c1ebe0771
Jiho Choi <withj1123@gmail.com>
2024-10-18 22:57:30 +0900

    프로젝트 구현하기 06 07 결제 실행하기

--- apps/order/src/app.module.ts
+++ apps/order/src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import * as Joi from 'joi';
 import { MongooseModule } from "@nestjs/mongoose";
 import { ClientsModule, Transport } from "@nestjs/microservices";
-import { PRODUCT_SERVICE, USER_SERVICE } from "@app/common";
+import { PAYMENT_SERVICE, PRODUCT_SERVICE, USER_SERVICE } from "@app/common";
 
 @Module({
     imports: [
@@ -16,6 +16,10 @@ import { PRODUCT_SERVICE, USER_SERVICE } from "@app/common";
                 USER_HOST: Joi.string().required(),
                 USER_TCP_PORT: Joi.number().required(),
                 DB_URL: Joi.string().required(),
+                PRODUCT_HOST: Joi.string().required(),
+                PRODUCT_TCP_PORT: Joi.number().required(),
+                PAYMENT_HOST: Joi.string().required(),
+                PAYMENT_TCP_PORT: Joi.number().required()
             })
         }),
         MongooseModule.forRootAsync({
@@ -28,7 +32,7 @@ import { PRODUCT_SERVICE, USER_SERVICE } from "@app/common";
             clients: [
                 {
                     name: USER_SERVICE,
-                    useFactory: (configService: ConfigService)=> ({
+                    useFactory: (configService: ConfigService) => ({
                         transport: Transport.TCP,
                         options: {
                             host: configService.getOrThrow<string>('USER_HOST'),
@@ -39,11 +43,22 @@ import { PRODUCT_SERVICE, USER_SERVICE } from "@app/common";
                 },
                 {
                     name: PRODUCT_SERVICE,
-                    useFactory: (configService: ConfigService)=> ({
+                    useFactory: (configService: ConfigService) => ({
                         transport: Transport.TCP,
                         options: {
                             host: configService.getOrThrow<string>('PRODUCT_HOST'),
                             port: configService.getOrThrow<number>('PRODUCT_TCP_PORT'),
+                        }
+                    }),
+                    inject: [ConfigService]
+                },
+                {
+                    name: PAYMENT_SERVICE,
+                    useFactory: (configService: ConfigService) => ({
+                        transport: Transport.TCP,
+                        options: {
+                            host: configService.getOrThrow<string>('PAYMENT_HOST'),
+                            port: configService.getOrThrow<number>('PAYMENT_TCP_PORT'),
                         }
                     }),
                     inject: [ConfigService]
