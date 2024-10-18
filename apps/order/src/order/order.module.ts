aec5b50fcd4267556a95cb40aacae535c7f0d205
Jiho Choi <withj1123@gmail.com>
2024-10-18 15:17:10 +0900

    프로젝트 구현하기 03 03 Order Microservice Entity 생성하기

---
+++ apps/order/src/order/order.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [],
+  controllers: [OrderController],
+  providers: [OrderService],
+})
+export class OrderModule { }
