342b4905906901c870d629db8b45e175c2ae5a8c
Jiho Choi <withj1123@gmail.com>
2024-11-22 13:07:28 +0900

    배포 03 05 Github CICD 생성하기

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-gateway -f ./apps/gateway/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-notification -f ./apps/notification/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-order -f ./apps/order/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-payment -f ./apps/payment/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-product -f ./apps/product/Dockerfile --target production .
-docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-user -f ./apps/user/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-gateway:latest -f ./apps/gateway/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-notification:latest -f ./apps/notification/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-order:latest -f ./apps/order/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-payment:latest -f ./apps/payment/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-product:latest -f ./apps/product/Dockerfile --target production .
+docker buildx build --platform linux/amd64,linux/arm64 -t codefactoryofficial/fc-nestjs-user:latest -f ./apps/user/Dockerfile --target production .
 
 docker push codefactoryofficial/fc-nestjs-gateway:latest
 docker push codefactoryofficial/fc-nestjs-notification:latest
